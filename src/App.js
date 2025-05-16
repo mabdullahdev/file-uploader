fb330b9dd1d95a7eb8027ccf537ecd19e708e26d
Abdullah <mabdullahkhan009@gmail.com>
2025-05-16 10:30:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileUploader from './components/FileUploader';
+
+function App() {
+  return (
+    <React.Fragment>
+      <FileUploader />
+    </React.Fragment>
+  );
+}
+
+export default App;
